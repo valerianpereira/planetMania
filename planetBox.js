ca5f703344d84e7e7f5de1c7a722f0e294179b68
valerianpereira <valerianpereira25@gmail.com>
2017-12-25 12:22:43 +0530

    Add info to the storage

--- planetBox.js
+++ planetBox.js
@@ -1,16 +1,16 @@
 // Holds all the information
 
 module.exports = {
-    "sun" : "Sun is stong and mighty",
-    "moon" : "Sun is stong and mighty",
-    "earth" : "Sun is stong and mighty",
-    "mercury" : "Sun is stong and mighty",
-    "venues" : "Sun is stong and mighty",
-    "earth" : "Sun is stong and mighty",
-    "mars" : "Sun is stong and mighty",
-    "jupiter": "Moon is calm",
-    "saturn": "Nearest to the earth",
-    "uranus": "Nearest to the earth",
-    "pluto": "Nearest to the earth",
-    "default": "Default response"
+    "sun" : "The sun lies at the heart of the solar system, where it is by far the largest object. It holds 99.8 percent of the solar system's mass and is roughly 109 times the diameter of the Earth — about one million Earths could fit inside the sun",
+    "moon" : "The Moon (or Luna) is the Earth's only natural satellite and was formed 4.6 billion years ago around some 30–50 million years after the formation of the solar system. The Moon is in synchronous rotation with Earth meaning the same side is always facing the Earth.",
+    "earth" : "The Earth's rotation is gradually slowing. Earth has a powerful magnetic field. The Earth was once believed to be the centre of the universe. There is only one natural satellite of the planet Earth.",
+    "mercury" : "Mercury is the closest planet to the Sun and is also the smallest of the eight planets in our solar system. For every 2 orbits of the Sun, which takes around 88 Earth days, Mercury completes three rotations of its axis. It is gravitationally locked and this rotation is unique to the solar system.",
+    "venus" : "Venus is the second planet from the Sun and is the second brightest object in the night sky after the Moon. Named after the Roman goddess of love and beauty, Venus is the second largest terrestrial planet and is sometimes referred to as the Earth's sister planet due the their similar size and mass.",
+    "mars" : "Mars is the fourth planet from the Sun and last of the terrestrial planets and is around 227,940,000 km from the Sun.The landmass of Mars and Earth is very similar. The planet is named after Mars, the Roman god of war.",
+    "jupiter": "The planet Jupiter is the fifth planet out from the Sun, and is two and a half times more massive than all the other planets in the solar system combined. It is made primarily of gases and is therefore known as a “gas giant”",
+    "saturn": "Saturn is the sixth planet from the Sun, and last of the planets known to ancient civilizations. Saturn is one of five planets able to be seen with the naked eye. In Roman mythology Saturn was the father of Jupiter, king of the gods.",
+    "uranus": "Uranus turns on its axis once every 17 hours, 14 minutes. Uranus makes one trip around the Sun every 84 Earth years. Uranus is often referred to as an “ice giant” planet. Uranus hits the coldest temperatures of any planet.",
+    "neptune": "Neptune is the eighth planet from the Sun making it the most distant in the solar system. This gas giant planet may have formed much closer to the Sun in early solar system history before migrating to its present position",
+    "pluto": "Pluto is named after the Greek god of the underworld. Pluto has five known moons. Pluto is the largest dwarf planet. Pluto is one third water",
+    "default": "Cannot find information about the planet you mentioned. Could you please ask for another one"
 };
