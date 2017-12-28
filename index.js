131e08be7420648f0330c1d40b82472fe912692a
valerianpereira <valerianpereira25@gmail.com>
2017-12-28 21:29:44 +0530

    Update the exit keyword

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ var handlers = {
     },
     'AMAZON.HelpIntent': function(){
         // Help me!!!!
-        var speechOutput = "You can say tell me about the sun or any other planet name or you can say exit..";
+        var speechOutput = "You can say tell me about the sun or any other planet name or you can say stop instead.";
         var reprompt = "What can I help you with ?";
         this.emit(":ask", speechOutput, reprompt);
     },
